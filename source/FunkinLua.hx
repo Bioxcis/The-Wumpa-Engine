ad25b953cb3caaf928635824068ed93f61a3d24a
Bioxcis-dono <102638604+Bioxcis@users.noreply.github.com>
2023-09-29 09:29:18 -0300

    Update Camera Follow Pos

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1579,10 +1579,16 @@ class FunkinLua {
 			PlayState.instance.moveCamera(isDad);
 			return isDad;
 		});
-		Lua_helper.add_callback(lua, "CameraFollowPos", function(targetX:Float, targetY:Float) {
-			PlayState.instance.camFollow.x = targetX;
-			PlayState.instance.camFollow.y = targetY;
-			PlayState.instance.setCameraForced(true);
+		Lua_helper.add_callback(lua, "CameraFollowPos", function(?camX:Dynamic, ?camY:Dynamic) {
+			if(caX == null || camY == null) {
+				PlayState.instance.triggerEventNote("Camera Follow Pos", "", "");
+				return false;
+			} else {
+				var targetX:String = string(camX);
+				var targetY:String = string(camY);
+				PlayState.instance.triggerEventNote("Camera Follow Pos", targetX, targetY);
+				return true;
+			}
 		});
 		Lua_helper.add_callback(lua, "cameraShake", function(camera:String, intensity:Float, duration:Float) {
 			cameraFromString(camera).shake(intensity, duration);
