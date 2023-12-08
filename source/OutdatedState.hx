388aeae068c70df4397bf06586f0e69563915f76
Bioxcis-dono <102638604+Bioxcis@users.noreply.github.com>
2023-12-07 22:08:40 -0300

    Outdated

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -27,13 +27,13 @@ class OutdatedState extends MusicBeatState
 
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
+			outdated version of Wumpa Engine (" + MainMenuState.wumpaEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			Press ESCAPE to proceed anyway.\n
 			\n
 			Thank you for using the Engine!",
 			32);
-		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		warnText.setFormat("CCZoinks Regular", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
 	}
