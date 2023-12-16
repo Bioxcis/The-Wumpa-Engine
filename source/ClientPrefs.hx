11078c440d71f090d8d6a6bee7e0a42138c44618
Bioxcis-dono <102638604+Bioxcis@users.noreply.github.com>
2023-12-16 14:07:41 -0300

    Mania Assets Update

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -21,12 +21,24 @@ class ClientPrefs {
 	public static var framerate:Int = 60;
 	public static var cursing:Bool = true;
 	public static var violence:Bool = true;
+	public static var flashWarning:Bool = true;
 	public static var camZooms:Bool = true;
 	public static var hideHud:Bool = false;
 	public static var hideWatermark:Bool = false;
 	public static var hideScoreText:Bool = false;
 	public static var noteOffset:Int = 0;
-	public static var arrowHSV:Array<Array<Int>> = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+	//public static var arrowHSV:Array<Array<Int>> = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+	public static var arrowHSV:Array<Array<Int>> = [
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0],
+		[0, 0, 0], [0, 0, 0]
+	];
 	public static var imagesPersist:Bool = false;
 	public static var ghostTapping:Bool = false;
 	public static var timeBarType:String = 'Tempo Restante';
@@ -52,9 +64,14 @@ class ClientPrefs {
 	public static var removePerfects:Bool = false;
 	public static var characterTrail:Bool = false;
 	public static var checkForUpdates:Bool = true;
+
+	// Set in Lua for Crash Night Funkin Mod
 	public static var tntrules:Bool = false;
 	public static var nitrorules:Bool = false;
 	public static var trialrules:Bool = false;
+
+	public static var convertEK:Bool = true;
+	public static var showKeybindsOnStart:Bool = true;
 	public static var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
 		'scrolltype' => 'multiplicativo', 
@@ -84,13 +101,197 @@ class ClientPrefs {
 	public static var badWindow:Int = 135;
 	public static var safeFrames:Float = 10;
 
-	//Every key has two binds, add your key bind down here and then add your control on options/ControlsSubState.hx and Controls.hx
+	// Every key has two bindings, add your default key binding here.
 	public static var keyBinds:Map<String, Array<FlxKey>> = [
-		//Key Bind, Name for ControlsSubState
+		// 1K
+		'note_one1'		=> [SPACE, G],
+		// 2K
+		'note_two1'		=> [D, LEFT],
+		'note_two2'		=> [L, RIGHT],
+		// 3K
+		'note_three1'	=> [D, LEFT],
+		'note_three2'	=> [SPACE, NONE],
+		'note_three3'	=> [L, RIGHT],
+		// 4K
 		'note_left'		=> [A, LEFT],
 		'note_down'		=> [S, DOWN],
 		'note_up'		=> [W, UP],
 		'note_right'	=> [D, RIGHT],
+		// 5K
+		'note_five1'	=> [A, LEFT],
+		'note_five2'	=> [S, DOWN],
+		'note_five3'	=> [SPACE, G],
+		'note_five4'	=> [K, UP],
+		'note_five5'	=> [L, RIGHT],
+		// 6K
+		'note_six1'		=> [A, Q],
+		'note_six2'		=> [S, W],
+		'note_six3'		=> [D, E],
+		'note_six4'		=> [J, I],
+		'note_six5'		=> [K, O],
+		'note_six6'		=> [L, P],
+		// 7K
+		'note_seven1'	=> [A, Q],
+		'note_seven2'	=> [S, W],
+		'note_seven3'	=> [D, E],
+		'note_seven4'	=> [SPACE, G],
+		'note_seven5'	=> [J, I],
+		'note_seven6'	=> [K, O],
+		'note_seven7'	=> [L, P],
+		// 8K
+		'note_eight1'	=> [A, Q],
+		'note_eight2'	=> [S, W],
+		'note_eight3'	=> [D, E],
+		'note_eight4'	=> [F, R],
+		'note_eight5'	=> [H, U],
+		'note_eight6'	=> [J, I],
+		'note_eight7'	=> [K, O],
+		'note_eight8'	=> [L, P],
+		// 9K
+		'note_nine1'	=> [A, Q],
+		'note_nine2'	=> [S, W],
+		'note_nine3'	=> [D, E],
+		'note_nine4'	=> [F, R],
+		'note_nine5'	=> [SPACE, G],
+		'note_nine6'	=> [H, U],
+		'note_nine7'	=> [J, I],
+		'note_nine8'	=> [K, O],
+		'note_nine9'	=> [L, P],
+		// 10K
+		'note_ten1'		=> [A, Q],
+		'note_ten2'		=> [S, W],
+		'note_ten3'		=> [D, E],
+		'note_ten4'		=> [F, R],
+		'note_ten5'		=> [G, T],
+		'note_ten6'		=> [SPACE, Y],
+		'note_ten7'		=> [H, U],
+		'note_ten8'     => [J, I],
+		'note_ten9'		=> [K, O],
+		'note_ten10'	=> [L, P],
+		// 11K
+		'note_elev1'	=> [A, Q],
+		'note_elev2'	=> [S, W],
+		'note_elev3'	=> [D, E],
+		'note_elev4'	=> [F, R],
+		'note_elev5'	=> [G, T],
+		'note_elev6'	=> [SPACE, NONE],
+		'note_elev7'	=> [H, Y],
+		'note_elev8'    => [J, U],
+		'note_elev9'	=> [K, I],
+		'note_elev10'	=> [L, O],
+		'note_elev11'	=> [PERIOD, P],
+		// 12K
+		'note_twel1'	=> [A, NONE],
+		'note_twel2'	=> [S, NONE],
+		'note_twel3'	=> [D, NONE],
+		'note_twel4'	=> [F, NONE],
+		'note_twel5'	=> [C, NONE],
+		'note_twel6'	=> [V, NONE],
+		'note_twel7'	=> [N, NONE],
+		'note_twel8'    => [M, NONE],
+		'note_twel9'	=> [H, NONE],
+		'note_twel10'	=> [J, NONE],
+		'note_twel11'	=> [K, NONE],
+		'note_twel12'	=> [L, NONE],
+		// 13K
+		'note_thir1'	=> [A, NONE],
+		'note_thir2'	=> [S, NONE],
+		'note_thir3'	=> [D, NONE],
+		'note_thir4'	=> [F, NONE],
+		'note_thir5'	=> [C, NONE],
+		'note_thir6'	=> [V, NONE],
+		'note_thir7'	=> [SPACE, NONE],
+		'note_thir8'	=> [N, NONE],
+		'note_thir9'    => [M, NONE],
+		'note_thir10'	=> [H, NONE],
+		'note_thir11'	=> [J, NONE],
+		'note_thir12'	=> [K, NONE],
+		'note_thir13'	=> [L, NONE],
+		// 14K
+		'note_fourt1'	=> [A, NONE],
+		'note_fourt2'	=> [S, NONE],
+		'note_fourt3'	=> [D, NONE],
+		'note_fourt4'	=> [F, NONE],
+		'note_fourt5'	=> [C, NONE],
+		'note_fourt6'	=> [V, NONE],
+		'note_fourt7'	=> [T, NONE],
+		'note_fourt8'    => [Y, NONE],
+		'note_fourt9'	=> [N, NONE],
+		'note_fourt10'	=> [M, NONE],
+		'note_fourt11'	=> [H, NONE],
+		'note_fourt12'	=> [J, NONE],
+		'note_fourt13'	=> [K, NONE],
+		'note_fourt14'	=> [L, NONE],
+		// 15K
+		'note_151'		=> [A, NONE],
+		'note_152'		=> [S, NONE],
+		'note_153'		=> [D, NONE],
+		'note_154'		=> [F, NONE],
+		'note_155'		=> [C, NONE],
+		'note_156'		=> [V, NONE],
+		'note_157'		=> [T, NONE],
+		'note_158'    	=> [Y, NONE],
+		'note_159'    	=> [U, NONE],
+		'note_1510'		=> [N, NONE],
+		'note_1511'		=> [M, NONE],
+		'note_1512'		=> [H, NONE],
+		'note_1513'		=> [J, NONE],
+		'note_1514'		=> [K, NONE],
+		'note_1515'		=> [L, NONE],
+		// 16K
+		'note_161'		=> [A, NONE],
+		'note_162'		=> [S, NONE],
+		'note_163'		=> [D, NONE],
+		'note_164'		=> [F, NONE],
+		'note_165'		=> [Q, NONE],
+		'note_166'		=> [W, NONE],
+		'note_167'		=> [E, NONE],
+		'note_168'    	=> [R, NONE],
+		'note_169'    	=> [Y, NONE],
+		'note_1610'		=> [U, NONE],
+		'note_1611'		=> [I, NONE],
+		'note_1612'		=> [O, NONE],
+		'note_1613'		=> [H, NONE],
+		'note_1614'		=> [J, NONE],
+		'note_1615'		=> [K, NONE],
+		'note_1616'		=> [L, NONE],
+		// 17K
+		'note_171'		=> [A, NONE],
+		'note_172'		=> [S, NONE],
+		'note_173'		=> [D, NONE],
+		'note_174'		=> [F, NONE],
+		'note_175'		=> [Q, NONE],
+		'note_176'		=> [W, NONE],
+		'note_177'		=> [E, NONE],
+		'note_178'    	=> [R, NONE],
+		'note_179'		=> [SPACE, NONE],
+		'note_1710'    	=> [Y, NONE],
+		'note_1711'		=> [U, NONE],
+		'note_1712'		=> [I, NONE],
+		'note_1713'		=> [O, NONE],
+		'note_1714'		=> [H, NONE],
+		'note_1715'		=> [J, NONE],
+		'note_1716'		=> [K, NONE],
+		'note_1717'		=> [L, NONE],
+		// 18K
+		'note_181'		=> [A, NONE],
+		'note_182'		=> [S, NONE],
+		'note_183'		=> [D, NONE],
+		'note_184'		=> [F, NONE],
+		'note_185'		=> [SPACE, NONE],
+		'note_186'		=> [H, NONE],
+		'note_187'		=> [J, NONE],
+		'note_188'		=> [K, NONE],
+		'note_189'  	=> [L, NONE],
+		'note_1810' 	=> [Q, NONE],
+		'note_1811'		=> [W, NONE],
+		'note_1812'		=> [E, NONE],
+		'note_1813'		=> [R, NONE],
+		'note_1814'		=> [T, NONE],
+		'note_1815'		=> [Y, NONE],
+		'note_1816'		=> [U, NONE],
+		'note_1817'		=> [I, NONE],
+		'note_1818'		=> [O, NONE],
 		
 		'ui_left'		=> [A, LEFT],
 		'ui_down'		=> [S, DOWN],
@@ -109,12 +310,13 @@ class ClientPrefs {
 		'debug_1'		=> [SEVEN, NONE],
 		'debug_2'		=> [EIGHT, NONE]
 	];
-	public static var defaultKeys:Map<String, Array<FlxKey>> = null;
+	//public static var defaultKeys:Map<String, Array<FlxKey>> = null;
+	public static var defaultKeys:Map<String, Array<FlxKey>> = keyBinds;
 
-	public static function loadDefaultKeys() {
-		defaultKeys = keyBinds.copy();
-		//trace(defaultKeys);
-	}
+	// public static function loadDefaultKeys() {
+	// 	defaultKeys = keyBinds.copy();
+	// 	//trace(defaultKeys);
+	// }
 
 	public static function saveSettings() {
 		FlxG.save.data.downScroll = downScroll;
@@ -127,6 +329,7 @@ class ClientPrefs {
 		FlxG.save.data.framerate = framerate;
 		//FlxG.save.data.cursing = cursing;
 		//FlxG.save.data.violence = violence;
+		FlxG.save.data.flashWarning = flashWarning;
 		FlxG.save.data.camZooms = camZooms;
 		FlxG.save.data.colorblindMode = colorblindMode;
 		FlxG.save.data.opponentStrums = opponentStrums;
@@ -174,11 +377,13 @@ class ClientPrefs {
 		FlxG.save.data.pauseMusic = pauseMusic;
 		FlxG.save.data.noteSkinSettings = noteSkinSettings;
 		FlxG.save.data.checkForUpdates = checkForUpdates;
+		FlxG.save.data.convertEK = convertEK;
+		FlxG.save.data.showKeybindsOnStart = showKeybindsOnStart;
 	
 		FlxG.save.flush();
 
 		var save:FlxSave = new FlxSave();
-		save.bind('controls_v2', 'ninjamuffin99'); //Placing this in a separate save so that it can be manually deleted without removing your Score and stuff
+		save.bind('controls_v2', 'bioxcis_dono'); //Placing this in a separate save so that it can be manually deleted without removing your Score and stuff
 		save.data.customControls = keyBinds;
 		save.flush();
 		FlxG.log.add("Settings saved!");
@@ -252,6 +457,9 @@ class ClientPrefs {
 		if(FlxG.save.data.violence != null) {
 			violence = FlxG.save.data.violence;
 		}*/
+		if(FlxG.save.data.flashWarning != null) {
+			flashWarning = FlxG.save.data.flashWarning;
+		}
 		if(FlxG.save.data.camZooms != null) {
 			camZooms = FlxG.save.data.camZooms;
 		}
@@ -343,6 +551,14 @@ class ClientPrefs {
 		if(FlxG.save.data.cameramoveonnotes != null) {
 			cameramoveonnotes = FlxG.save.data.cameramoveonnotes;
 		}
+		if (FlxG.save.data.convertEK != null)
+		{
+			convertEK = FlxG.save.data.convertEK;
+		}
+		if (FlxG.save.data.showKeybindsOnStart != null)
+		{
+			showKeybindsOnStart = FlxG.save.data.showKeybindsOnStart;
+		}
 		if(FlxG.save.data.underlaneVisibility != null) {
 			underlaneVisibility = FlxG.save.data.underlaneVisibility;
 		}
@@ -379,7 +595,7 @@ class ClientPrefs {
 		}
 
 		var save:FlxSave = new FlxSave();
-		save.bind('controls_v2', 'ninjamuffin99');
+		save.bind('controls_v2', 'bioxcis_dono');
 		if(save != null && save.data.customControls != null) {
 			var loadedControls:Map<String, Array<FlxKey>> = save.data.customControls;
 			for (control => keys in loadedControls) {
