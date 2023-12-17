78f864605ef69a2e55eaae2d2d6ecb26b2f46d22
Bioxcis-dono <102638604+Bioxcis@users.noreply.github.com>
2023-12-17 17:01:16 -0300

    Status options

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -43,78 +43,10 @@ class VisualsUISubState extends BaseOptionsMenu
 		addOption(option);
 
 		var option:Option = new Option('Splash da Nota',
-			"Se marcado, ao acertar as notas \"Sick!\" vai mostrar efeitos de splash.",
+			"Se marcado, as notas \"Whoa!\" e \"Nsano!\" vão mostrar\nefeitos de splash ao serem acertadas.",
 			'noteSplashes',
 			'bool',
 			true);
-		addOption(option);
-		/*
-		var option:Option = new Option('Esconder HUD',
-			'Se marcado, oculta a maioria dos elementos do HUD.',
-			'hideHud',
-			'bool',
-			false);
-		addOption(option);
-		*/
-		var option:Option = new Option("Modo Exibicao",
-			'Se marcado, oculta todo o HUD e ativa o Botplay.',
-			'showcaseMode',
-			'bool',
-			false);
-		addOption(option);
-
-		var option:Option = new Option('Esconder Texto de Exibicao',
-			'Se marcado, oculta o texto de exibição no canto inferior esquerdo\ndurante a reprodução da música.',
-			'hideWatermark',
-			'bool',
-			false);
-		addOption(option);
-		/*
-		var option:Option = new Option('Trilha no Personagem',
-			'Gerava o efeito de trilha do Espirito de Thorns...\n!Disponivel apenas no Modo de Edição de Notas!',
-			'characterTrail',
-			'bool',
-			false);
-		addOption(option);
-		
-		var option:Option = new Option('Esconder Ponts',
-			'Se marcada, vai ocultar o texto de pontuação, precisão e erros\nno inferior da HUD nas músicas.',
-			'hideScoreText',
-			'bool',
-			false);
-		addOption(option);
-		*/
-		var option:Option = new Option('Animacao do Icone',
-			'Clássico igual aos ícones do FNF original / Especial da Wumpa Engine.',
-			'iconbops',
-			'string',
-			'OS',
-			['Especial', 'Classico']);
-		addOption(option);
-
-		/*var option:Option = new Option('Posicao do Texto Ponts',
-			'Classico igual posicao do Psych Engine, Novo igual a posicao do OS Engine.',
-			'scoreposition',
-			'string',
-			'Classico',
-			['Classico', 'Novo']);			sla :/
-		addOption(option);
-		*/
-		var option:Option = new Option('Filtro Daltonico',
-			'Definir um filtro daltônico\n(torna o jogo mais acessível para pessoas daltônicas).',
-			'colorblindMode',
-			'string',
-			'Nada', 
-			['Nada', 'Deuteranopia', 'Protanopia', 'Tritanopia']);
-		option.onChange = ColorblindFilters.applyFiltersOnGame;
-		addOption(option);
-		
-		var option:Option = new Option('Barra de Tempo:',
-			"Como você quer a sua barra de tempo?",
-			'timeBarType',
-			'string',
-			'Tempo Restante',
-			['Tempo Restante', 'Tempo Recorrido', 'Nome da Musica', 'OS Tempo Restante', 'Desativado']);
 		addOption(option);
 
 		var option:Option = new Option('Zoom da Camera',
@@ -124,46 +56,34 @@ class VisualsUISubState extends BaseOptionsMenu
 			true);
 		addOption(option);
 
-		var option:Option = new Option('Zoom no Texto Ponts no hit',
-			"Se marcado, ativa o zoom do Texto de Pontuação\nsempre que você toca uma nota.",
+		var option:Option = new Option('Zoom dos Pontos da Partida',
+			"Se marcado, ativa o zoom do Texto de Classificação\nsempre que você acerta uma nota.",
 			'scoreZoom',
 			'bool',
 			true);
 		addOption(option);
-		/*
-		var option:Option = new Option('Novo Tamanho Hud',
-			"Se marcado, a HUD do jogo será alterada\npara um novo tamanho!",
+
+		var option:Option = new Option('Tamanho da Hud',
+			"Se marcado, a HUD do jogo será alterada\npara um tamanho menor.",
 			'hudSize',
 			'bool',
 			true);
 		addOption(option);
-		*/
-		var option:Option = new Option('Ativar Shaders',
-			"Se marcado, as shaders estarão ativadas.\nDesative essa opção caso seu computador rejeite as shaders.",
-			'shadersActive',
-			'bool',
-			true);
-		addOption(option);
 
-		var option:Option = new Option('Ativar Julgamento',
-			"Se marcado, todo julgamento de nota será contado e\napresentado na HUD.",
-			'showStatus',
+		var option:Option = new Option("Modo Exibicao",
+			'Se marcado, oculta todo o HUD e ativa o Botplay,\nentrando no modo de exibição de músicas e cinemáticas',
+			'showcaseMode',
 			'bool',
 			false);
 		addOption(option);
 
-		var option:Option = new Option('Transparencia da Vida',
-			'Quão transparente deve ser a barra de vida e os ícones.',
-			'healthBarAlpha',
-			'percent',
-			1);
-		option.scrollSpeed = 1.6;
-		option.minValue = 0.0;
-		option.maxValue = 1;
-		option.changeValue = 0.1;
-		option.decimals = 1;
+		var option:Option = new Option('Esconder Dados da Musica',
+			'Se marcado, oculta o texto com dados da música atual\nno canto inferior esquerdo dos estágios.',
+			'hideWatermark',
+			'bool',
+			false);
 		addOption(option);
-		
+
 		#if !mobile
 		var option:Option = new Option('Contador de FPS',
 			'Se marcado, exibe o contador de FPS.',
@@ -173,6 +93,73 @@ class VisualsUISubState extends BaseOptionsMenu
 		addOption(option);
 		option.onChange = onChangeFPSCounter;
 		#end
+
+		var option:Option = new Option('Movimento dos Icones',
+			'Clássico será igual aos ícones do FNF original\nEspecial terá uma animação da Wumpa Engine.',
+			'iconbops',
+			'string',
+			'OS',
+			['Especial', 'Classico']);
+		addOption(option);
+
+		var option:Option = new Option('Barra de Tempo:',
+			"Altera o estilo da Barra de Tempo no jogo.",
+			'timeBarType',
+			'string',
+			'Tempo Restante',
+			['Tempo Restante', 'Tempo Recorrido', 'Nome da Musica', 'Desativado']);
+		addOption(option);
+
+		var option:Option = new Option('Filtro Daltonico',
+			'Define um filtro daltônico no jogo\n(torna o jogo mais acessível para pessoas daltônicas).',
+			'colorblindMode',
+			'string',
+			'Nada', 
+			['Nada', 'Deuteranopia', 'Protanopia', 'Tritanopia']);
+		option.onChange = ColorblindFilters.applyFiltersOnGame;
+		addOption(option);
+
+		var option:Option = new Option('Transparencia da Vida',
+			'Altera a transparência da barra de vida e os ícones.',
+			'healthBarAlpha',
+			'percent',
+			1);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+
+		var option:Option = new Option('Mostrar Teclas das Notas',
+			"Se marcado, as teclas correspondentes serão mostradas abaixo das notas no inicio da música.",
+			'showKeybindsOnStart',
+			'bool',
+			true);
+		addOption(option);
+
+		//Normally shaders work on certain computers, but on some computers they don't, I don't understand shaders...
+		//if you want to help with this, check with the mod developer
+		var option:Option = new Option('Ativar Shaders',
+			"Se marcado, as shaders estarão ativadas.\nDesative essa opção caso tenha problemas com as shaders.",
+			'shadersActive',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option('Ativar Julgamento',
+			"Se marcado, todo julgamento de nota será contado e apresentado na HUD\njunto do seu maior combo.",
+			'showStatus',
+			'bool',
+			false);
+		addOption(option);
+
+		var option:Option = new Option('Cores Julgamento',
+			"Se marcado, os julgamentos na HUD terão cores,\ncaso contrário vair ser tudo branco.",
+			'colorStatus',
+			'bool',
+			false);
+		addOption(option);
 
 		/*var option:Option = new Option('Plano de Fundo Tematico do Menu Principal',
 			'Se marcado, a cor de fundo do menu principal depende da hora do dia.',
@@ -189,7 +176,6 @@ class VisualsUISubState extends BaseOptionsMenu
 			false);
 		option.defaultValue = false;
 		addOption(option);
-		*/
 
 		var option:Option = new Option('Skin de Nota',
 			"Qual aparência você quer para suas notas?",
@@ -198,7 +184,38 @@ class VisualsUISubState extends BaseOptionsMenu
 			'Classico',
 			['Classico', 'Circulo']);
 		addOption(option);
-		/*
+
+		var option:Option = new Option('Esconder HUD',
+			'Se marcado, oculta a maioria dos elementos do HUD.',
+			'hideHud',
+			'bool',
+			false);
+		addOption(option);
+
+		// Disponivel apenas no Modo de Edição de Notas!
+		var option:Option = new Option('Trilha no Personagem',
+			'Gerava o efeito de trilha do Espirito de Thorns...',
+			'characterTrail',
+			'bool',
+			false);
+		addOption(option);
+
+		var option:Option = new Option('Esconder Ponts',
+			'Se marcada, vai ocultar o texto de pontuação, precisão e erros\nno inferior da HUD nas músicas.',
+			'hideScoreText',
+			'bool',
+			false);
+		addOption(option);
+
+		var option:Option = new Option('Posicao do Texto Ponts',
+			'Classico igual posicao do Psych Engine, Novo igual a posicao do OS Engine.',
+			'scoreposition',
+			'string',
+			'Classico',
+			['Classico', 'Novo']);
+		addOption(option);
+
+		// Custom Musics added
 		var option:Option = new Option('Musica do Pause:',
 			"Que música você prefere que toque ao pausar?",
 			'pauseMusic',
