66b56ab5d6b7895dafb19b27283e40143e5b0fcd
Bioxcis-dono <102638604+Bioxcis@users.noreply.github.com>
2023-12-17 17:02:19 -0300

    idk

--- source/stages/objects/DadBattleFog.hx
+++ source/stages/objects/DadBattleFog.hx
@@ -9,25 +9,25 @@ class DadBattleFog extends FlxSpriteGroup
 	{
 		super();
 		
-		alpha = 0;
+		//alpha = 0;
 		blend = ADD;
 
 		var offsetX = 200;
-		var smoke:BGSprite = new BGSprite('smoke', -1550 + offsetX, 660 + FlxG.random.float(-20, 20), 1.2, 1.05);
-		smoke.setGraphicSize(Std.int(smoke.width * FlxG.random.float(1.1, 1.22)));
-		smoke.updateHitbox();
-		smoke.velocity.x = FlxG.random.float(15, 22);
-		smoke.active = true;
-		smoke.antialiasing = ClientPrefs.globalAntialiasing;
-		add(smoke);
+		var smoke1:BGSprite = new BGSprite('smoke', -1550 + offsetX, 660 + FlxG.random.float(-20, 20), 1.2, 1.05);
+		smoke1.setGraphicSize(Std.int(smoke1.width * FlxG.random.float(1.1, 1.22)));
+		smoke1.updateHitbox();
+		smoke1.velocity.x = FlxG.random.float(15, 22);
+		smoke1.active = true;
+		smoke1.antialiasing = ClientPrefs.globalAntialiasing;
+		add(smoke1);
 
-		var smoke:BGSprite = new BGSprite('smoke', 1550 + offsetX, 660 + FlxG.random.float(-20, 20), 1.2, 1.05);
-		smoke.setGraphicSize(Std.int(smoke.width * FlxG.random.float(1.1, 1.22)));
-		smoke.updateHitbox();
-		smoke.velocity.x = FlxG.random.float(-15, -22);
-		smoke.active = true;
-		smoke.flipX = true;
-		smoke.antialiasing = ClientPrefs.globalAntialiasing;
-		add(smoke);
+		var smoke2:BGSprite = new BGSprite('smoke', 1550 + offsetX, 660 + FlxG.random.float(-20, 20), 1.2, 1.05);
+		smoke2.setGraphicSize(Std.int(smoke2.width * FlxG.random.float(1.1, 1.22)));
+		smoke2.updateHitbox();
+		smoke2.velocity.x = FlxG.random.float(-15, -22);
+		smoke2.active = true;
+		smoke2.flipX = true;
+		smoke2.antialiasing = ClientPrefs.globalAntialiasing;
+		add(smoke2);
 	}
 }
